92d744fac72e86eae5dc6d313213656b0484c068
Rahul-Katari <99799585+Rahul-Katari@users.noreply.github.com>
2024-02-28 16:47:30 +0530

    added redirection for about

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
- window.onload = function() {
-const currenturl =window.location.href
-// if(!currenturl.includes('maintenance')){
-//  window.location.href = '/maintenance.html';
-// }    
-    };
+window.onload = function () {
+    //  to redirect users to the new URL after a delay
+    const currenturl = window.location.href
+    if (currenturl.includes('about')) {
+        window.location.href = '/advanced-sap-fico-training-hyderabad.html';
+    }
+};
+
 (function ($) {
     "use strict";
 
@@ -16,8 +18,8 @@ const currenturl =window.location.href
         }, 1);
     };
     spinner();
-    
-    
+
+
     // Initiate the wowjs
     new WOW().init();
 
@@ -30,36 +32,36 @@ const currenturl =window.location.href
             $('.sticky-top').css('top', '-100px');
         }
     });
-    
-    
+
+
     // Dropdown on mouse hover
     const $dropdown = $(".dropdown");
     const $dropdownToggle = $(".dropdown-toggle");
     const $dropdownMenu = $(".dropdown-menu");
     const showClass = "show";
-    
-    $(window).on("load resize", function() {
+
+    $(window).on("load resize", function () {
         if (this.matchMedia("(min-width: 992px)").matches) {
             $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
+                function () {
+                    const $this = $(this);
+                    $this.addClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "true");
+                    $this.find($dropdownMenu).addClass(showClass);
+                },
+                function () {
+                    const $this = $(this);
+                    $this.removeClass(showClass);
+                    $this.find($dropdownToggle).attr("aria-expanded", "false");
+                    $this.find($dropdownMenu).removeClass(showClass);
+                }
             );
         } else {
             $dropdown.off("mouseenter mouseleave");
         }
     });
-    
-    
+
+
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
@@ -70,7 +72,7 @@ const currenturl =window.location.href
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
     });
 
@@ -78,19 +80,19 @@ const currenturl =window.location.href
         $(".back-to-top").addClass("d-none");
         console.log("working");
     });
-    
+
 
 
     // Header carousel
     $(".header-carousel").owlCarousel({
         autoplay: true,
-        autoplayTimeout:15000,
+        autoplayTimeout: 15000,
         smartSpeed: 1500,
         items: 1,
         dots: false,
         loop: true,
-        nav : true,
-        navText : [
+        nav: true,
+        navText: [
             '<i class="bi bi-chevron-left"></i>',
             '<i class="bi bi-chevron-right"></i>'
         ]
@@ -105,20 +107,20 @@ const currenturl =window.location.href
         margin: 24,
         dots: true,
         loop: true,
-        nav : false,
+        nav: false,
         responsive: {
-            0:{
-                items:1
+            0: {
+                items: 1
             },
-            768:{
-                items:2
+            768: {
+                items: 2
             },
-            992:{
-                items:3
+            992: {
+                items: 3
             }
         }
     });
 
-    
-    
+
+
 })(jQuery);
