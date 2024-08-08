57fc737a2684a492b0dfedf739c5dc7b4789f740
Rahul Katari <rahulkatari99@gmail.com>
2024-08-08 10:32:00 +0530

    added validation for mobile number in contact form.

--- js/main.js
+++ js/main.js
@@ -130,23 +130,24 @@ function addModal() {
             method="POST">
               <div class="form-floating mb-3">
   <input type="text" class="form-control" id="name" name="name" placeholder="Bat Man" required>
-  <label for="name">Name</label>
+  <label for="name">Name*</label>
 </div>
 <div class="form-floating mb-3">
-  <input type="tel" class="form-control" id="mobile" name="mobile" placeholder="+91 99999 99999" required>
-  <label for="mobile">Mobile Number</label>
+  <input type="tel" class="form-control" id="mobile" name="mobile" placeholder="+91 99999 99999" required oninput="validateMobileNumber(this)">
+  <label for="mobile">Mobile Number*</label>
 </div>
 <div class="form-floating">
               <div class="form-floating mb-3">
   <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
-  <label for="email">Email address</label>
+  <label for="email">Email address*</label>
 </div>
 <div class="form-floating">
                                     <textarea class="form-control" placeholder="Leave a message here" name="message" style="height: 100px"></textarea>
                                     <label for="message">Message</label>
                                 </div>
+                            <p id="enquire-form-status" class="text-danger"></p>
 <div class="d-flex justify-content-end mt-3">
-<button class="w-auto rounded-3 btn btn-primary w-100 px-5" id="contact-form-button" data-bs-dismiss="modal" aria-label="Close">Submit</button>
+<button class="w-auto rounded-3 btn btn-primary w-100 px-5" id="enquire-form-button" aria-label="Close">Submit</button>
 </div>
 </div>
             </div>
@@ -162,24 +163,71 @@ function addModal() {
   $("#enquire-modal").modal("show");
 }
 
+function validateMobileNumber(input) {
+  const value = input.value;
+  // Remove non-digit characters and limit to 10 digits
+  input.value = value.replace(/\D/g, '').slice(0, 10);
+}
+
 // Function to check if the modal should be shown
 function checkModal() {
   var showModal = localStorage.getItem("showModal");
 
   // If showModal is null or false, show the modal and set the flag to true
   if (!showModal) {
-    // setTimeout(addModal, 10000); // Show modal after 10 seconds
-    // $("#myForm").submit(function (event) {
-    //   handleSubmit(event);
-    // });
     setTimeout(function () {
       addModal();
-      $("#myForm").submit(function (event) {
+      $("#enquire-modal-form").submit(function (event) {
         handleSubmit(event);
       });
     }, 10000);
-    localStorage.setItem("showModal", true);
   }
+
+  async function handleSubmit(event) {
+    let form = document.getElementById("enquire-modal-form");
+    event.preventDefault();
+    var status = document.getElementById("enquire-form-status");
+    var submit = document.getElementById("enquire-form-button");
+    submit.setAttribute("disabled", true);
+    submit.innerHTML = '...';
+    var data = new FormData(event.target);
+    let mobile = data.get('mobile');
+    if (mobile.length === 10 && ["9", "8", '7', '6'].some(num => mobile.startsWith(num))) {
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+          'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your submission!";
+          form.reset();
+          submit.removeAttribute('disabled');
+          submit.innerHTML = 'Submit';
+          window.alert('Thank you for your Submission');
+          $("#enquire-modal").modal("hide");
+          localStorage.setItem("showModal", true);
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    else {
+      status.innerHTML = "Oops! Please Enter a Valid Mobile Number"
+      submit.removeAttribute('disabled');
+      submit.innerHTML = 'Submit';
+    }
+  }
+  // form.addEventListener("submit", handleSubmit)
 }
 // Function to handle form submission
 function handleSubmit(event) {
